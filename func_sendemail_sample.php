e63b547c5d376997e294cb90af145792fe5a5c4b
CansCurtis <Curtis@CansCurtis.com>
2024-05-13 18:37:51 +0800

    Fix garbled chars problem for zh-TW

--- func_sendemail_sample.php
+++ func_sendemail_sample.php
@@ -24,23 +24,27 @@ function sendemail_sample($sender_email, $sender_name, $recipient_email, $recipi
     // SMTP Configs - Global Parameters
     global $smtp_host, $smtp_port, $smtp_username, $smtp_password;
 
+    // UTF-8 Encode
+    date_default_timezone_set('Asia/Taipei');
+    mb_internal_encoding('UTF-8');
+
     //Create an instance; passing `true` enables exceptions
     $mail = new PHPMailer(true);
 
     try {
         //Server settings
-        //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                    //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = $smtp_host;                             //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = $smtp_username;                         //SMTP username
         $mail->Password   = $smtp_password;                         //SMTP password
-        //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;          //Enable implicit TLS encryption
         $mail->Port       = $smtp_port;                             //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
-        $mail->setFrom($sender_email, $sender_name);
-        $mail->addAddress($recipient_email, $recipient_name);     //Name is optional
+        $mail->setFrom($sender_email, mb_encode_mimeheader($sender_name,"UTF-8"));
+        $mail->addAddress($recipient_email, mb_encode_mimeheader($recipient_name,"UTF-8"));  //Name is optional
         //$mail->addReplyTo('info@example.com', 'Information');
         //$mail->addCC('cc@example.com');
         //$mail->addBCC('bcc@example.com');
@@ -51,7 +55,7 @@ function sendemail_sample($sender_email, $sender_name, $recipient_email, $recipi
 
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = $subject;
+        $mail->Subject = mb_encode_mimeheader($subject,"UTF-8");
         $mail->Body    = $body;
 
         $mail->send();
